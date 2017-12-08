fb0f3e2a88a0ef5c11bfefce69bb07853ed56ec8
bszabo <bszabo@wenetto.hu>
2017-12-08 20:16:54 +0100

    added user entity service

--- components/services/dburl/src/index.js
+++ components/services/dburl/src/index.js
@@ -4,19 +4,20 @@ const config = require('./config');
 const app = express();
 app.use(express.json());
 
-app.get('/dburl/collection/:collection', function (req, res) {
-    var collection = req.params.collection;
-
-    try {
-        res.json(config.get('database.collections.' + collection));
-    } catch (err) {
-        res.sendStatus(404);
-    }
-});
 
 app.get('/dburl', function (req, res) {
     var url = 'mongodb://' + config.get('database.uri') + '/' + config.get('database.database-name');
-    res.json(url);
+    res.status(200).json(url);
 });
+
+// app.get('/dburl/collection/:collection', function (req, res) {
+//     var collection = req.params.collection;
+
+//     try {
+//         res.json(config.get('database.collections.' + collection));
+//     } catch (err) {
+//         res.sendStatus(404);
+//     }
+// });
 
 app.listen(config.get('port'));
---
+++ components/services/user/src/index.js
@@ -0,0 +1,60 @@
+const config = require('./config');
+
+const express = require('express');
+const got = require('got');
+var mongoClient = require('mongodb').MongoClient;
+
+var app = express();
+app.use(express.json());
+
+var getDbUrl = function (url) {
+    return got(url + '/dburl')
+        .then(res => {
+            return res.body;
+        })
+        .catch(err => {
+            throw {
+                status: 404,
+                payload: {
+                    message: "Could not retrieve database information."
+                }
+            };
+        });
+};
+
+app.get('/user/:username', function (req, res) {
+    var query = { username: req.params.username };
+    var dbToClose;
+
+
+    mongoClient.connect("mongodb://localhost:27017/pretender_database", function (err, db) {
+        db.collection('users');
+    });
+
+    getDbUrl('http://' + config.get('database-url.uri'))
+        .then(url => {
+            var user = null;
+            url = url.replace(/"/g, '');
+            mongoClient.connect(url)
+                .then(db => {
+                    dbToClose = db;
+                    return db.collection('users');
+                })
+                .then(coll => {
+                    return coll.findOne(query);
+                })
+                .then(result => {
+                    res.status(200).json(result);
+                })
+                .catch(err => {
+                    console.log(err)
+                    res.status(400).json(err.message);
+                });
+        })
+        .catch(err => {
+            res.status(err.status)
+                .json(err.payload);
+        });
+});
+
+app.listen(config.get('port'));
