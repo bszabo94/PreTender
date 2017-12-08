2337f191d9a6461e863b9ada62ad0d88622472f3
bszabo <bszabo@wenetto.hu>
2017-12-08 13:51:55 +0100

    removed deprecated architecture

--- src/js/url.js
+++
@@ -1,7 +0,0 @@
-const PROTOCOL = "mongodb://";
-
-module.exports = {
-    getURL: function(address, port, database){
-        return PROTOCOL + address.toString() + ":" + port.toString() + "/" + database.toString(); 
-    }
-};
