6cbfc26179ddff229703c2bca874f0f5896637ce
bszabo <bszabo@wenetto.hu>
2017-12-11 00:08:26 +0100

    added apply tender task service

---
+++ components/services/applytender/src/config.js
@@ -0,0 +1,53 @@
+const convict = require('convict');
+
+const config = convict({
+    port: {
+        doc: "The service will listen on this port.",
+        format: 'port',
+        default: '9033',
+        ENV: 'APPLY_TENDER_PORT'
+    },
+    'user-url': {
+        uri: {
+            doc: 'The URI to the User Entity Service.',
+            format: String,
+            default: 'localhost:9002',
+            env: 'APPLY_TENDER_USER_URI'
+        }
+    },
+    'tender-type-url': {
+        uri: {
+            doc: 'The URI to the Tender Entity Service.',
+            format: String,
+            default: 'localhost:9003',
+            env: 'APPLY_TENDER_TENDER_TYPE_URI'
+        }
+    },
+    'issued-tender-url': {
+        uri: {
+            doc: 'The URI to the Tender Entity Service.',
+            format: String,
+            default: 'localhost:9004',
+            env: 'APPLY_TENDER_ISSUED_TENDER_URI'
+        }
+    },
+    'user-exists-check-url': {
+        uri: {
+            doc: 'The URI to the User Existence Check Microservice.',
+            format: String,
+            default: 'localhost:9010',
+            env: 'APPLY_TENDER_USER_EXISTS_CHECK_URI'
+        }
+    },
+    'save-application-url': {
+        uri: {
+            doc: 'The URI to the Save Application Microservice.',
+            format: String,
+            default: 'localhost:9012',
+            env: 'APPLY_TENDER_SAVE_APPLICATION_URI'
+        }
+    }
+});
+
+config.validate({ allowed: 'strinct' });
+module.exports = config;
---
+++ components/services/applytender/src/index.js
@@ -0,0 +1,216 @@
+const config = require('./config');
+
+const express = require('express');
+const got = require('got');
+
+var app = express();
+app.use(express.json());
+
+var newApplication = {
+    "referenceID": 0,
+    "tendertype": 0,
+    "user": "",
+    "lastedited": "",
+    "finilized": 0,
+    "data": null
+};
+
+var getDbUrl = function (url) {
+    return got(url + '/dburl')
+        .then(res => {
+            return res.body;
+        })
+        .catch(err => {
+            throw {
+                status: 404,
+                payload: {
+                    message: "Could not retrieve database information."
+                }
+            };
+        });
+};
+
+var checkUserExists = function (username) {
+    return got('http://' + config.get('user-exists-check-url.uri') + '/checkuserexists/' + username)
+        .then(resp => {
+
+            if (JSON.parse(resp.body).status) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        .catch(() => {
+            throw {
+                status: 400,
+                payload: {
+                    message: "Could not use user-exist service."
+                }
+            }
+        });
+};
+
+var getTenderType = function (type) {
+    return got('http://' + config.get('tender-type-url.uri') + '/tendertype', { headers: { 'type': type } })
+        .catch(() => {
+            throw {
+                status: 400,
+                payload: {
+                    message: "Could not retrieve tender-type."
+                }
+            }
+        });
+};
+
+var getTenderTypeByID = function (id) {
+    return got('http://' + config.get('tender-type-url.uri') + '/tendertype/' + id)
+        .catch(() => {
+            throw {
+                status: 400,
+                payload: {
+                    message: "Could not retrieve tender-type."
+                }
+            }
+        });
+};
+
+var getIssuedtender = function (id) {
+    return got('http://' + config.get('issued-tender-url.uri') + '/issuedtender/' + id)
+        .catch(() => {
+            throw {
+                status: 400,
+                payload: {
+                    message: "Could not retrieve issued tender."
+                }
+            }
+        });
+};
+
+var modUser = function (username, query) {
+    return got.post('http://' + config.get('user-url.uri') + '/user/' + username, { json: true, body: query })
+        .catch(err => {
+            console.log(err)
+            throw {
+                status: 400,
+                payload: {
+                    message: "Could not modify user properlyr."
+                }
+            }
+        });
+};
+
+var saveApplication = function (application) {
+    return got.post('http://' + config.get('save-application-url.uri') + '/saveapplication', { json: true, body: application })
+        .catch(err => {
+            // console.log(err);
+            throw {
+                status: 400,
+                payload: {
+                    message: "Could not save application."
+                }
+            }
+        });
+};
+
+app.post('/applytender/:username/:issuedtenderid', function (req, res) {
+    var username = req.params.username,
+        issuedtenderid = req.params.issuedtenderid;
+
+    checkUserExists(username)
+        .then(exists => {
+            if (exists) {
+                getIssuedtender(issuedtenderid)
+                    .then(it => {
+                        it = JSON.parse(it.body);
+                        if (it == null) {
+                            res.status(400).json("Issued Tender not found.");
+                        }
+                        getTenderType(it.type)
+                            .then(tt => {
+                                tt = JSON.parse(tt.body);
+                                if (it == null) {
+                                    res.status(400).json("Tender Type not found.");
+                                }
+
+                                newApplication.user = username;
+                                newApplication.referenceID = issuedtenderid;
+                                newApplication.tendertype = tt['_id'];
+                                var date = new Date()
+                                newApplication.lastedited = date.toISOString();
+
+                                var data = {
+                                    firstname: "",
+                                    lastname: ""
+                                };
+                                var ttData = tt.requirements;
+                                for (prop of Object.keys(ttData))
+                                    data[prop] = "";
+
+                                data["recommendation"] = "";
+                                data["documents"] = [];
+                                newApplication.data = data;
+                                newApplication = newApplication;
+                                saveApplication(newApplication)
+                                    .then(result => {
+
+                                        var appID = result.body.insertedId;
+                                        // console.log(appID);
+
+                                        var query = {
+                                            $push: {
+                                                tenders: appID
+                                            }
+                                        };
+
+                                        // query = JSON.parse(JSON.stringify(query));
+                                        console.log(query);
+
+                                        modUser(username, query)
+                                            .then(result => {
+                                                console.log("DBG3")
+                                                if (result.modifiedCount == 1) {
+                                                    res.status(200)
+                                                        .json("Tender succesfully applied.");
+                                                } else {
+                                                    console.log("WAAAT")
+                                                    res.status(400)
+                                                        .json("Something went wrong. Apply unsuccesful.");
+                                                }
+                                            })
+                                            .catch(err => {
+                                                // console.log(err);
+                                                res.status(400)
+                                                    .json(err.message);
+                                            });
+                                    })
+                                    .catch(err => {
+                                        // console.log(err)
+                                        res.status(400)
+                                            .json(err.message);
+                                    });
+                            })
+                            .catch(err => {
+                                // console.log(err)
+                                res.status(400)
+                                    .json(err.message);
+                            });
+
+                    })
+                    .catch(err => {
+                        res.status(400)
+                            .json(err.message);
+                    });
+            } else {
+                res.status(400)
+                    .json("Invalid username for apply tender.");
+            }
+        })
+        .catch(() => {
+            res.status(400)
+                .json("Could not check user existence.");
+        });
+
+
+});
+
+app.listen(config.get('port'));
