08ba7739abd84b3858d4eb1ea0d422f79c05a268
bszabo <bszabo@wenetto.hu>
2017-12-10 16:43:09 +0100

    added save user capability to user entity service

--- components/services/user/src/config.js
+++ components/services/user/src/config.js
@@ -14,6 +14,14 @@ const config = convict({
             default: 'localhost:9001',
             env: 'USER_GET_DATABASE_URL_URI'
         }
+    },
+    'user-exists-check-url': {
+        uri: {
+            doc: 'The URI to the User Existence Check Microservice.',
+            format: String,
+            default: 'localhost:9010',
+            env: 'USER_USER_EXISTS_CHECK_URI'
+        }
     }
 });
 
--- components/services/user/src/index.js
+++ components/services/user/src/index.js
@@ -22,6 +22,25 @@ var getDbUrl = function (url) {
         });
 };
 
+var checkUserExists = function (username) {
+    return got('http://' + config.get('user-exists-check-url.uri') + '/checkuserexists/' + username)
+        .then(resp => {
+            if (JSON.parse(resp.body).status) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        .catch(() => {
+            throw {
+                status: 400,
+                payload: {
+                    message: "Could not use user-exist service."
+                }
+            }
+        });
+};
+
 app.get('/user/:username', function (req, res) {
     var query = { username: req.params.username };
     var dbToClose;
@@ -50,6 +69,59 @@ app.get('/user/:username', function (req, res) {
             res.status(err.status)
                 .json(err.payload);
         });
+});
+
+app.post('/user/:username', function (req, res) {
+    if (JSON.stringify(req.body) == '{}') {
+        res.status(400)
+            .json("Nothing to update.");
+    } else {
+        checkUserExists(req.params.username)
+            .then(exists => {
+                if (exists) {
+                    var dbToClose, query = req.body;
+                    getDbUrl('http://' + config.get('database-url.uri'))
+                        .then(url => {
+                            url = url.replace(/"/g, '');
+                            mongoClient.connect(url)
+                                .then(db => {
+                                    dbToClose = db;
+                                    return db.collection('users');
+                                })
+                                .then(coll => {
+                                    return coll.updateOne({ "username": req.params.username }, { $set: query });
+                                })
+                                .then(result => {
+                                    dbToClose.close();
+                                    res.status(200).json(result);
+                                })
+                                .catch(err => {
+                                    dbToClose.close();
+                                    res.status(400).json(err.message);
+                                });
+                        })
+                        .catch(err => {
+                            throw {
+                                status: 400,
+                                payload: {
+                                    message: "Something went wrong while saving " + req.params.username + " user."
+                                }
+                            };
+                        });
+                } else {
+                    throw {
+                        status: 400,
+                        payload: {
+                            message: "Something went wrong. Username " + req.params.username + " does not exists in the database."
+                        }
+                    };
+                }
+            })
+            .catch(err => {
+                res.status(400)
+                    .json("Something went wrong while saving user.");
+            });
+    };
 });
 
 app.listen(config.get('port'));
