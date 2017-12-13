ef8aa3f36a9d09fb4d678a1948d7a81dd09f8688
bszabo <bszabo@wenetto.hu>
2017-12-13 18:37:05 +0100

    updated config of user

--- components/services/user/src/config.js
+++ components/services/user/src/config.js
@@ -5,21 +5,12 @@ const config = convict({
         doc: "The service will listen on this port.",
         format: 'port',
         default: '9002',
-        ENV: 'USER_PORT'
     },
     'database': {
         url: {
             doc: 'Address of the database.',
             format: String,
             default: 'mongodb://127.0.0.1:27017/pretender',
-        }
-    },
-    'user-exists-check-url': {
-        uri: {
-            doc: 'The URI to the User Existence Check Microservice.',
-            format: String,
-            default: 'localhost:9010',
-            env: 'USER_USER_EXISTS_CHECK_URI'
         }
     }
 });
