a2a5cec956fa1bf182d59bd9bb679c214a5d4dc5
bszabo <bszabo@wenetto.hu>
2017-12-13 18:31:36 +0100

    added query all capability, and some refactor for prettier code

--- components/services/issuedtender/src/index.js
+++ components/services/issuedtender/src/index.js
@@ -1,34 +1,52 @@
 const config = require('./config');
 
-const express = require('express');
-const got = require('got');
-var mongoClient = require('mongodb');
-var cors = require('cors');
+const express = require('express'),
+    got = require('got'),
+    mongoClient = require('mongodb'),
+    cors = require('cors'),
+    app = express();
 
-var app = express();
 app.use(express.json());
 app.use(cors());
 
 app.get('/issuedtender/:id', function (req, res) {
     var query = { _id: parseInt(req.params.id) };
-    console.log(query);
-    var dbToClose;
+    var dataBase;
 
     mongoClient.connect(config.get('database.url'))
         .then(db => {
-            dbToClose = db;
-            return db.collection('issuedTenders');
-        })
-        .then(coll => {
-            return coll.findOne(query);
+            dataBase = db;
+            return db.collection('issuedTenders').findOne(query);
         })
         .then(result => {
-            dbToClose.close();
-            res.status(200).json(result);
+            dataBase.close();
+            res.status(200)
+                .json(result);
         })
         .catch(err => {
-            dbToClose.close();
-            res.status(400).json(err.message);
+            dataBase.close();
+            res.status(404)
+                .json(err.message);
+        });
+});
+
+app.get('/issuedtender', function (req, res) {
+    var dataBase;
+
+    mongoClient.connect(config.get('database.url'))
+        .then(db => {
+            dataBase = db;
+            return db.collection('issuedTenders').find({});
+        })
+        .then(result => {
+            dataBase.close();
+            res.status(200)
+                .json(result);
+        })
+        .catch(err => {
+            dataBase.close();
+            res.status(404)
+                .json(err.message);
         });
 });
 
